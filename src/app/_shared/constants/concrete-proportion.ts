12cf738428e4b40b4af4065be85a09b418be938d
sharmane burlaza <sjmburlaza@gmail.com>
2023-04-10 21:59:48 +0800

    Add concrete proportion data

---
+++ src/app/_shared/constants/concrete-proportion.ts
@@ -0,0 +1,34 @@
+export const concreteProportions = [
+    {
+        class: 'AA',
+        proportion: [1, 1.5, 3],
+        CIB40kg: 12,
+        CIB50kg: 9.5,
+        'sand-cum': 0.5,
+        'gravel-cum': 1.0
+    },
+    {
+        class: 'A',
+        proportion: [1, 2, 4],
+        CIB40kg: 9,
+        CIB50kg: 7,
+        'sand-cum': 0.5,
+        'gravel-cum': 1.0
+    },
+    {
+        class: 'B',
+        proportion: [1, 2.5, 5],
+        CIB40kg: 7.5,
+        CIB50kg: 6,
+        'sand-cum': 0.5,
+        'gravel-cum': 1.0
+    },
+    {
+        class: 'C',
+        proportion: [1, 3, 6],
+        CIB40kg: 6,
+        CIB50kg: 5,
+        'sand-cum': 0.5,
+        'gravel-cum': 0.5
+    },
+]
